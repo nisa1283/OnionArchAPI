cff04427008513dbd56790b13d79a81b743f388c
nisa1283 <nisaunal21@istanbularel.edu.tr>
2026-04-27 14:38:15 +0300

    read repository yapıldı

---
+++ Core/OnionAPI.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionAPI.Application.Interfaces.Repositories
+{
+    public interface IWriteRepository
+    {
+    }
+}
